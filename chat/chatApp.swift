ecca092f8a2b3f219f5da27b1973ab1d325a5210
hashil <semin>
2021-07-17 16:48:44 +0530

    Initial Commit

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by Hashil semin on 17/07/21.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
